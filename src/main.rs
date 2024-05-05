4364f1b2549f7058191233a12cfdadd60124e885
Abhi10699 <patelabhi10699@gmail.com>
2024-05-05 16:47:58 -0400

    feat: added cli support

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-use image::{self, imageops, Rgb, RgbImage};
+use clap::Parser;
+use image::{self, imageops, ImageBuffer, Rgb, RgbImage};
+
+// Simple tool to overlay images on a white backdrop
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Input Image Path
+    #[arg(short, long)]
+    input: String,
+
+    /// Output Image Path
+    #[arg(short, long)]
+    output: String,
+}
 
 #[derive(Debug)]
 struct Position {
@@ -7,70 +21,71 @@ struct Position {
 }
 
 #[derive(Debug)]
-struct Object {
+struct ImageDimension {
     w: i32,
     h: i32,
 }
 
-fn find_center_position(background: Object, foreground: Object) -> Position {
+impl ImageDimension {
+    fn from_image_buffer(image: &ImageBuffer<Rgb<u8>, Vec<u8>>) -> ImageDimension {
+        ImageDimension {
+            w: image.width() as i32,
+            h: image.height() as i32,
+        }
+    }
+}
+
+fn find_center_position(background: ImageDimension, foreground: ImageDimension) -> Position {
     // assume they are both aligned at top left (0,0)
     let background_center = Position {
         x: background.w / 2,
         y: background.h / 2,
     };
-    let c = Position {
+    Position {
         x: ((background_center.x - foreground.w) + foreground.w / 2),
         y: ((background_center.y - foreground.h) + foreground.h / 2),
-    };
-
-    c
+    }
 }
 
 fn main() {
-    let mut image_file = image::open("./test.jpg").unwrap().into_rgb8();
-    
-    let aspect_ratio: f32 = image_file.height() as f32 / image_file.width() as f32;
+    let cli_args = Args::parse();
+
+    let mut foreground_image = image::open(cli_args.input).unwrap().into_rgb8();
+
+    let aspect_ratio: f32 = foreground_image.height() as f32 / foreground_image.width() as f32;
 
     println!(
         "Height: {height}, Width: {width}, aspect_ratio: {ar}",
-        height = image_file.height(),
-        width = image_file.width(),
+        height = foreground_image.height(),
+        width = foreground_image.width(),
         ar = aspect_ratio,
     );
 
     let mut white_image = RgbImage::from_pixel(1080, 1920, Rgb { 0: [255, 255, 255] });
-    
-    let new_size = Object {
+
+    let new_size = ImageDimension {
         w: 970,
-        h: (970.0 * aspect_ratio) as i32
-    };
-    
-    
-    image_file = imageops::resize(&image_file, new_size.w as u32, new_size.h as u32, imageops::FilterType::CatmullRom);
-
-
-
-    let background = Object {
-        w: white_image.width() as i32,
-        h: white_image.height() as i32,
+        h: (970.0 * aspect_ratio) as i32,
     };
 
-    let foreground = Object {
-        w: image_file.width() as i32,
-        h: image_file.height() as i32,
-    };
+    foreground_image = imageops::resize(
+        &foreground_image,
+        new_size.w as u32,
+        new_size.h as u32,
+        imageops::FilterType::CatmullRom,
+    );
 
-    println!("{:?}", foreground);
-    println!("{:?}", background);
+    let background = ImageDimension::from_image_buffer(&white_image);
+
+    let foreground = ImageDimension::from_image_buffer(&foreground_image);
 
     let center_pos = find_center_position(background, foreground);
-    println!("{:?}", center_pos);
 
     imageops::overlay(
         &mut white_image,
-        &mut image_file,
+        &mut foreground_image,
         center_pos.x.into(),
         center_pos.y.into(),
     );
-    white_image.save("output.png").unwrap();
+    white_image.save(cli_args.output + "output.png").unwrap();
 }
